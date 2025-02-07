dd03f1d24e7930f1fdb3fdc679e8c7e962fdfa3b
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-02-07 16:38:38 +0700

    first commit

---
+++ Sample.cs
@@ -0,0 +1,15 @@
+﻿class Solution
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Viết mã của bạn ở đây
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Đã xảy ra lỗi: {ex.Message}");
+        }
+    }
+}
