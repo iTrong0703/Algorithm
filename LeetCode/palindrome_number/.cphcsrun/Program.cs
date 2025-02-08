178e0682925865ddd3bae33ba5060a7f4cff3918
Nguyen Thanh Trong <ngthtrong0354@gmail.com>
2025-02-08 14:26:38 +0700

    solved palindrome_number

---
+++ LeetCode/palindrome_number/.cphcsrun/Program.cs
@@ -0,0 +1,41 @@
+﻿class Solution
+{
+    public bool IsPalindrome(int x) 
+    {
+        List<int> lst = new List<int>();
+        if (x < 0) {
+            x = Math.Abs(x);
+            while (x != 0) {
+                lst.Add(x % 10);
+                x /= 10;
+            }
+            lst[0] *= -1;
+        }
+        else {
+            while (x != 0) {
+                lst.Add(x % 10);
+                x /= 10;
+            }
+        }
+        
+        if (lst.Count() > 1 && lst[0].Equals(lst.Last()))
+        {
+            return true;
+        }
+        return false;
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            int x = int.Parse(Console.ReadLine());
+            Solution s = new Solution();
+            var result = s.IsPalindrome(x);
+            System.Console.WriteLine(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Đã xảy ra lỗi: {ex.Message}");
+        }
+    }
+}
---
+++ LeetCode/palindrome_number/Program.cs
@@ -0,0 +1,44 @@
+﻿class Solution
+{
+    public bool IsPalindrome(int x) 
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+        if (x >= 0 && x < 10) {
+            return true;
+        }
+        List<int> lst = new List<int>();
+        while (x != 0) {
+            lst.Add(x % 10);
+            x /= 10;
+        }
+        int left = 0;
+        int right = lst.Count - 1;
+        while (left < right) 
+        {
+            if (!lst[left].Equals(lst[right]))
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            int x = int.Parse(Console.ReadLine());
+            Solution s = new Solution();
+            var result = s.IsPalindrome(x);
+            System.Console.WriteLine(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Đã xảy ra lỗi: {ex.Message}");
+        }
+    }
+}
